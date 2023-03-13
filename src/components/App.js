29246d8f389c018ae9fd3d7aff0d17f6d8f17f52
Francesca Bentin <francescabentin@gmail.com>
2023-03-13 10:11:30 +0100

    handle and render functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
 import '../styles/App.css';
+import data from '../data/data.json';
 
 
 
 
 function App() {
+  // variables de estado
+  const [quotes, setQuotes] = useState(data);
+  const [search, setSearch] = useState('');
+  const [character, setCharacter] = useState('todos');
+
+  //handlers
+
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  }
+
+  const handleSearch = (ev) => {
+    setSearch(ev.currentTarget.value);
+  }
+
+  const handleCharacter = (ev) => {
+    setCharacter(ev.target.value);
+  }
 
 
+  // renders
+  const renderQuotes = () => {
+    return quotes
+      .filter((eachQuote) => {
+        return (
+          eachQuote.quote.toLowerCase().includes(search.toLowerCase())
+        );
+      })
+      .filter((eachQuote) => {
+        return character === 'todos' || eachQuote.character.toLowerCase() === character.toLowerCase();
+      })
+      .map((eachQuote, i) => (
+        <li key={i}>
+          <p>{`${eachQuote.quote}-${eachQuote.character}`}</p>
+        </li>
+      ));
+  };
 
 
   return (
@@ -13,7 +50,7 @@ function App() {
     <div>
       <header>
         <h1>Frases de Friends</h1>
-        <form>
+        <form onSubmit={handleForm}>
           <label htmlFor='search'>Filtrar por frase: </label>
           <input
             className="header__search"
@@ -22,22 +59,27 @@ function App() {
             name="search"
             id='search'
             placeholder="buscar frase"
+            onInput={handleSearch}
           />
-          <label htmlFor='select'>filtrar por personage: </label>
-          <input
-            className="header__select"
-            autoComplete="off"
-            type="select"
-            name="select"
-            id='select'
-            placeholder="elegir"
-          />
+          <label className="fheader_select" htmlFor="select">
+            filtrar por personaje
+            <select onChange={handleCharacter} value={character} name="select" id="select">
+              <option value="todos">
+                todos
+              </option>
+              <option value="Joey">Joey</option>
+              <option value="Chandler">Chandler</option>
+              <option value="monica">monica</option>
+              <option value="Rachel">Rachel</option>
+              <option value="Ross">Ross</option>
+            </select>
+          </label>
         </form>
       </header>
       <main>
         <section>
           <ul>
-
+            {renderQuotes()}
           </ul>
         </section>
       </main>
